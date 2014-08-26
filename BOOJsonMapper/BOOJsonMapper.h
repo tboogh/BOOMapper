a2519a370a086f2cb253b8bc2b74169497923b8e
Tobias Boogh <tobias.boogh@bybrick.se>
2014-08-26 20:01:56 +0200

    Initial Commit

---
+++ BOOJsonMapper/BOOJsonMapper.h
@@ -0,0 +1,13 @@
+//
+//  BOOJsonMapper.h
+//  BOOJsonMapper
+//
+//  Created by Tobias Boogh on 26/08/14.
+//  Copyright (c) 2014 Tobias Boogh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BOOJsonMapper : NSObject
+
+@end
---
+++ BOOJsonMapper/BOOJsonMapper.m
@@ -0,0 +1,13 @@
+//
+//  BOOJsonMapper.m
+//  BOOJsonMapper
+//
+//  Created by Tobias Boogh on 26/08/14.
+//  Copyright (c) 2014 Tobias Boogh. All rights reserved.
+//
+
+#import "BOOJsonMapper.h"
+
+@implementation BOOJsonMapper
+
+@end
